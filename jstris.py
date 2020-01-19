66f45998a2d573625107effe7b84c89e0bbed288
meppydc <49413486+meppydc@users.noreply.github.com>
2020-01-19 16:45:41 -0500

    get_mode is mad

--- jstris.py
+++ jstris.py
@@ -1,4 +1,27 @@
+import requests
+import json
+import discord
 #jstris related features for the bot
+
+class ModeError(Exception):
+    pass
+
+class UsernameError(Exception):
+    pass
+
+class InvalidMode(Exception):
+    pass
+
+class Mode:
+    #keep parameter names consistent with jstris url
+    def __init__(self,name,play,*modes) :
+        self.name = name
+        self.play = play
+        self.modes = modes
+    
+    def get_mode(mode):
+        #raises ValueError
+        return modes.index(mode)
 
 jstris_modes = [
     [None],
@@ -10,27 +33,23 @@ jstris_modes = [
     ['Maps','no maps lol'], 
     ['Tsd','20TSD']]
 
-class Mode:
-    def __init__(self,mode,id,*types) :
-        self.mode = mode
-        self.id = id
-        self.types = types
-    
-    def get_type(type):
-        try:
-            return types.index(type)
-        except ValueError:
-            return None
+SPRINT = Mode('Sprint',1,['40L','20L','100L','1000L'])
+CHEESE = Mode('Cheese',3,['10L','18L','100L'])
+SURVIVAL = 'sur'
+ULTRA = 'ult'
+TSD = 'useless api'
 
-def get_records(username, play, type):
-    url = f'https://jstris.jezevec10.com/api/u/{username}/records/{play}?mode={type}'
+MODES = [None,SPRINT,'freeplay',CHEESE,SURVIVAL,ULTRA,'maps',TSD]
+
+def get_records(username, play, mode):
+    url = f'https://jstris.jezevec10.com/api/u/{username}/records/{play}?mode={mode}'
 
     r = requests.get(url)
     return r.json()
 
-def create_embed(username, play, type):
+def create_embed(username, play, mode):
 
-    r = get_records(username, play, type)
+    r = get_records(username, play, mode)
 
     try:
         #try tenth first because ones can't exist without tenths? not sure if it works that way
@@ -49,10 +68,10 @@ def create_embed(username, play, type):
     embed = discord.Embed(title=r['name'],
             colour=discord.Colour(0xe67e22),
             url=f"https://jstris.jezevec10.com/u/{r['name']}",
-            description=f"Displaying {jstris_modes[play][type]} {jstris_modes[play][0]} statistics for player {r['name']} on Jstris") 
+            description=f"Displaying {MODES[play]['modes'][mode]} {MODES[play]['name']} statistics for player {r['name']} on Jstris") 
     
-    embed.add_field(name="Best", value=r['min'] if play != 4 else r['max'], inline=True)
-    embed.add_field(name="Worst", value=r['max'] if play != 4 else r['min'], inline=True)
+    embed.add_field(name="Best", value=r['min'] if play != 4 and play != 5 else r['max'], inline=True)
+    embed.add_field(name="Worst", value=r['max'] if play != 4 and play != 5  else r['min'], inline=True)
     embed.add_field(name="Average", value=r['avg'], inline=True)
 
     embed.add_field(name="Total Games", value=r['games'], inline=True)
@@ -69,23 +88,39 @@ def create_embed(username, play, type):
 
     return embed
 
+def sprint(username,mode):
+    try:
+        modeIndex = SPRINT.get_mode(mode)
+    except ValueError:
+        raise ModeError('ModeError')
 
-class Jstris:
-    def __init__(self):
-        self.SPRINT = Mode('sprint',1,['40L','20L','100L','1000L'])
-        self.CHEESE = Mode('cheese',3,['10L','18L','100L'])
+    try:
+        embed = create_embed(username,SPRINT.play,modeIndex)
+    except:
+        raise UsernameError('UsernameError') 
 
-    def sprint(username,type):
-        return
+    return embed
 
-    def cheese(username,type):
-        return
+def cheese(username,type):
+    return
 
-    def survival(username):
-        return
+def survival(username):
+    return
 
-    def ultra(username):
-        return
+def ultra(username):
+    return
 
-    def tsd(username):
-        return
+def tsd(username):
+    return
+
+def embed_test():
+    embed = discord.Embed(title="title ~~(did you know you can have markdown here too?)~~", colour=discord.Colour(0x30298a), url="https://discordapp.com", description="this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```")
+
+    embed.set_footer(text="footer text", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
+
+    embed.add_field(name="🤔", value="some of these properties have certain limits...")
+    embed.add_field(name="🙄", value="an informative error should show up, and this view will remain as-is until all issues are fixed")
+    embed.add_field(name="<:pepega:667519589856313375>", value="these last two", inline=True)
+    embed.add_field(name="<:pepega:667519589856313375>", value="are inline fields", inline=True)
+
+    return embed
